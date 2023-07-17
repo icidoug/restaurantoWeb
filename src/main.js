d382c9803400f55dad05c18d3f6b59a01715befe
DmitriyMarkin <d@8up.ru>
2023-07-17 11:44:37 +0300

    partner cookie

--- src/main.js
+++ src/main.js
@@ -15,6 +15,9 @@ if(urlParams.has('table')) {
 if(urlParams.has('zone')) {
 	Cookies.set('zone', urlParams.get('zone'), { expires: 1 })
 }
+if(urlParams.has('partner')) {
+	Cookies.set('partner', urlParams.get('partner'), { expires: 1 })
+}
 
 Framework7.use(Framework7Vue);
 
@@ -23,6 +26,7 @@ const app = createApp(App)
 registerComponents(app);
 
 axios.defaults.withCredentials = true;
+axios.defaults.headers.common['partner'] = Cookies.get('partner') || '';
 
 app.use(VueAxios, axios)
 	.mount('#app');
