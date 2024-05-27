92c959746e1018af9f2d72dfb57637126e4501a6
DmitriyMarkin <d@8up.ru>
2024-05-27 13:38:53 +0300

    paydala

--- src/lib/workers/workerCheckOrder.js
+++ src/lib/workers/workerCheckOrder.js
@@ -4,6 +4,8 @@ import {f7} from "framework7-vue";
 export const workerCheckOrder = async (id) => {
 	const result = await store.dispatch('order/checkOrderPayment', id);
 	if (result?.is_paid) {
+		store.commit('order/setIsOpenPayDalaModal', false);
+		f7.views.current.router.navigate('/')
 		f7.popup.open('.order-payment-confirm-popup');
 		localStorage.lastOrderId = null;
 	} else {
