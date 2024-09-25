e353feed350270c818e49b54a286af6f4ab67c7c
DmitriyMarkin <d@8up.ru>
2024-09-25 11:14:13 +0300

    hide confirm order

--- src/views/basket/index.vue
+++ src/views/basket/index.vue
@@ -12,7 +12,21 @@
             <div class="padding-side">
                 <catalog-buy-with v-if="suitableItems && suitableItems.length > 0" :items="suitableItems"/>
             </div>
-            <basket-form v-if="items.length > 0"/>
+            <basket-form v-if="items.length > 0 && !partner.hide_confirm_order"/>
+            <div v-if="partner.hide_confirm_order && !partner.hide_call_waiter" class="basket-form__footer">
+                <f7-button class="btn btn--pink btn--arrow" popup-open=".waiter-popup">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2 16L22 16" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
+                        <path d="M4 19L20 19" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
+                        <path d="M20 16C20 10.4771 18.5 6 12 6C5.5 6 4 10.4772 4 16" stroke="white"
+                              stroke-width="1.5"/>
+                        <path
+                            d="M13.7324 6C13.9026 5.70583 14 5.36429 14 5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5C10 5.36429 10.0974 5.70583 10.2676 6"
+                            stroke="white" stroke-width="1.5"/>
+                    </svg>
+                    <span>{{ $t('call_waiter') }}</span>
+                </f7-button>
+            </div>
         </div>
         <catalog-card-popup v-if="items.length > 0"/>
     </f7-page>
@@ -41,12 +55,9 @@
             f7router: Object,
         },
         setup(props) {
-            //console.log('props', props.f7router)
-            const orderId = computed(() => {
-                return store.getters['order/Id']
+            const partner = computed(() => {
+                return store.getters['partner/partner']
             });
-
-
 
             onMounted(async () => {
                 await store.dispatch('basket/getItems');
@@ -62,7 +73,8 @@
 
             return {
                 items,
-                suitableItems
+                suitableItems,
+                partner
             };
         },
         mounted() {
